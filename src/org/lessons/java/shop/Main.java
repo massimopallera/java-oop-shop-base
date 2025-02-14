90546a2586e89d20717025552c80cc1d4ecb631b
massimopallera <pallera.massimo@gmail.com>
2025-02-14 17:46:50 +0100

    add access modifiers + new methods

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,15 +1,22 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        final int iva = 22; // cant be changed -- use the percentage without % :))
 
-        Prodotto cinquecento = new Prodotto("Fiat 500", "Golosa macchina da combattimento urbano", 5000.50, iva);
+        Prodotto.iva = new BigDecimal(.22);
 
-        System.out.println(cinquecento.getNomeEsteso());
-        System.out.println(cinquecento.getNomeEsteso());
-        System.out.println(cinquecento.getPrezzo());
-        System.out.println(cinquecento.getPrezzoConIva());
+        Prodotto cinquecento = new Prodotto("Fiat 500", "Golosa macchina da combattimento urbano", new BigDecimal(22000.65));
+        Prodotto vuoto = new Prodotto();
+
+        System.out.println(vuoto.getNomeEsteso());
+        System.out.println(vuoto.getPrezzo());
+        System.out.println(vuoto.getPrezzoConIva());
+        System.out.println(vuoto.getDescrizione());
+        System.out.println(vuoto.getCodice());
+        System.out.println(vuoto.getDescrizione());
+
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,16 +1,28 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Prodotto {
+    static BigDecimal iva = new BigDecimal(0);
 
-    int codice; // Random
-    String nome;
-    String descrizione;
-    double prezzo;
-    int iva;
+    private int codice; // Random
+    private String nome;
+    private String descrizione;
+    private BigDecimal prezzo;
 
-    Prodotto(String nome, String descrizione, double prezzo, int iva){
+    Prodotto(){
+
+        Random codice = new Random();
+
+        this.codice =  codice.nextInt(99999);
+        this.nome = null;
+        this.descrizione = null;
+        this.prezzo = null;
+    }
+
+    Prodotto(String nome, String descrizione, BigDecimal prezzo){
 
         Random codice = new Random();
 
@@ -18,20 +30,45 @@ public class Prodotto {
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
-        this.iva = iva;
 
     }
 
-    public double getPrezzo(){
-        return this.prezzo;
+    public BigDecimal getPrezzo(){
+        return (this.prezzo != null) ? this.prezzo.setScale(2, RoundingMode.FLOOR) : null;
     }
 
-    public double getPrezzoConIva(){
-        return this.prezzo+(this.prezzo*iva/100);
+    public void setPrezzo(BigDecimal prezzo){
+        //signum controlla il segno del valore prezzo, restituendo -1 se negativo, 0 se 0 e 1 se positivo
+        this.prezzo = prezzo.signum() == 1 ? prezzo : this.prezzo;
+    }
+    
+    public String getNome(){
+        return this.nome;
     }
 
+    public void setNome(String nome){
+        this.nome = !nome.equals("") ? nome : "Prodotto Senza Nome";
+    }
+    
+    public String getDescrizione(){
+        return this.descrizione;
+    }
+
+    public void setDescrizione(String descrizione){
+        this.descrizione = !descrizione.equals("") ? descrizione : "Prodotto Senza descrizione";
+    }
+
+    public int getCodice(){
+        return this.codice;
+    }
+    
+    // Metodi aggiuntivi
     public String getNomeEsteso(){        
-        return this.codice + "-" + this.nome.replaceAll("\\s","").trim();
+        return this.nome != null ? this.codice + "-" + this.nome.replaceAll("\\s","").trim() : null;
+    }
+
+    public BigDecimal getPrezzoConIva(){
+        return this.prezzo != null ? (this.prezzo.add(this.prezzo.multiply(iva))).setScale(2,RoundingMode.FLOOR) : null;
     }
 
 }
